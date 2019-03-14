cb20c96704d16321fe9ee94ca0c2896d684b5f3f
qhykwsw <1425751338@qq.com>
2019-03-15 00:23:14 +0800

    more elegant

--- engine/_gain_pagesize.py
+++ engine/_gain_pagesize.py
@@ -39,6 +39,7 @@ class GainPageSize(SpiderEngine):
                     log.error(f"# {idx+1}-{flag+1}: 提取IP失败")                        
                     ip_gene = self.get_ip()
                     continue
+
                 i = random.randint(1, 3)
                 time.sleep(i)
 
@@ -55,6 +56,10 @@ class GainPageSize(SpiderEngine):
                     continue
                 except requests.exceptions.ChunkedEncodingError as e:
                     log.error(f"# {idx+1}-{flag+1}: Connection broken: IncompleteRead")
+                    continue
+                except requests.exceptions.ContentDecodingError as e:
+                    log.error(f"# {idx+1}-{flag+1}: Received response with content-encoding: gzip, but failed to decode it.")
+                    continue
 
                 html.encoding = 'utf-8'
                 soup = BeautifulSoup(html.text, 'lxml')
@@ -80,14 +85,18 @@ class GainPageSize(SpiderEngine):
                     else:
                         log.error(f"# {idx+1}-{flag+1}: 被认为是机器人")  
                         continue
+
                 log.info(f' # {idx+1}-{flag+1}: {company} success\n')
                 self.spider_success+=1
                 self.spider_all+=1
+
                 with open(self.pklfile, 'wb') as f:
                     pickle.dump(self.results, f)
                 log.info(f" # {idx+1}-{flag+1}: 保存到文件")
+
                 t2 = time.time()
                 log.info(f' # 共耗时{round((t2-t1)/60,1)}分, 成功爬取了{self.spider_success}/{self.spider_all}家公司\n')
+                
                 idx += 1
                 flag = 0
             else:
