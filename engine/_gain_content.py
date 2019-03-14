8811c67b1c145934a2b85d0cb99c059701aa8c2a
wenzhiyin <43230896+wenzhiyin@users.noreply.github.com>
2019-03-14 20:27:18 +0800

    modified the if conditiong of  pagenow

--- engine/_gain_content.py
+++ engine/_gain_content.py
@@ -66,85 +66,87 @@ class GainContent(SpiderEngine):
             # if self.results[idx]['page_size'] == 0:
             pagenow = 1
             while True:
-                if self.results[idx]['patent'][pagenow] == []:
-                    try:
-                        ip = next(ip_gene)
-                        # print(ip)
-                        log.info(f" # {idx+1}-{pagenow}-{flag+1}: 提取IP成功: {ip['http']}")
-                    except:
-                        log.error(f"# {idx+1}-{pagenow}-{flag+1}: 提取IP失败")                        
-                        ip_gene = self.get_ip()
-                        continue
-                    i = random.randint(1, 3)
-                    time.sleep(i)
+                    #if self.results[idx]['patent'][pagenow] == []:
+                    if not pagenow in self.results[idx]['patent']:
+                        try:
+                            ip = next(ip_gene)
+                            # print(ip)
+                            log.info(f" # {idx+1}-{pagenow}-{flag+1}: 提取IP成功: {ip['http']}")
+                        except:
+                            log.error(f"# {idx+1}-{pagenow}-{flag+1}: 提取IP失败")                        
+                            ip_gene = self.get_ip()
+                            continue
+                        i = random.randint(1, 3)
+                        time.sleep(i)
 
-                    try:
-                        html = self.get_html(applicant=company, ip=ip, strSources=self.strSources,pagenow=pagenow)
-                    except requests.exceptions.ProxyError as e:
-                        log.error(f"# {idx+1}-{pagenow}-{flag+1}: 连接方在一段时间后没有正确答复或连接的主机没有反应，连接尝试失败")
-                        continue
-                    except requests.exceptions.ReadTimeout as e:
-                        log.error(f"# {idx+1}-{pagenow}-{flag+1}: ReadTimeout({self.timeout})")
-                        continue
-                    except requests.exceptions.ConnectionError as e:
-                        log.error(f"# {idx+1}-{pagenow}-{flag+1}: Connection aborted")
-                        continue
-                    except requests.exceptions.ChunkedEncodingError as e:
-                        log.error(f"# {idx+1}-{pagenow}-{flag+1}: Connection broken: IncompleteRead")
-                        continue
-                    html.encoding = 'utf-8'
-                    soup = BeautifulSoup(html.text, 'lxml')
-                    # print(soup)
-                    try:
-                        main_tb = soup.find("div", class_="next")
-                        posion = main_tb.find('input')['onkeypress']
-                        start = posion.find('zl_tz')+6
-                        page_size = int(posion[start:-1])
-                        self.results[idx]['page_size'] = page_size
+                        try:
+                            html = self.get_html(applicant=company, ip=ip, strSources=self.strSources,pageNow=pagenow)
+                        except requests.exceptions.ProxyError as e:
+                            log.error(f"# {idx+1}-{pagenow}-{flag+1}: 连接方在一段时间后没有正确答复或连接的主机没有反应，连接尝试失败")
+                            continue
+                        except requests.exceptions.ReadTimeout as e:
+                            log.error(f"# {idx+1}-{pagenow}-{flag+1}: ReadTimeout({self.timeout})")
+                            continue
+                        except requests.exceptions.ConnectionError as e:
+                            log.error(f"# {idx+1}-{pagenow}-{flag+1}: Connection aborted")
+                            continue
+                        except requests.exceptions.ChunkedEncodingError as e:
+                            log.error(f"# {idx+1}-{pagenow}-{flag+1}: Connection broken: IncompleteRead")
+                            continue
+                        html.encoding = 'utf-8'
+                        soup = BeautifulSoup(html.text, 'lxml')
+                        # print(soup)
+                        try:
+                            main_tb = soup.find("div", class_="next")
+                            posion = main_tb.find('input')['onkeypress']
+                            start = posion.find('zl_tz')+6
+                            page_size = int(posion[start:-1])
+                            self.results[idx]['page_size'] = page_size
+                            
+                        except:
+                            if soup.find("h1", class_="head_title") == None:
+                                log.error(f"# {idx+1}-{pagenow}-{flag+1}: 没有您要查询的结果")
+                                flag+=1
+                                if flag >= self.trytimes:
+                                    self.spider_all+=1
+                                    log.info(f' # {idx+1}-{pagenow}-{flag}: {company} failed')
+                                    idx += 1
+                                    flag = 0
+                                    t2 = time.time()
+                                    log.info(f' # 耗时{round((t2-t1),1)}seconds, 成功爬取了{self.spider_success}/{self.spider_all}家公司\n')
+                                    #wy break;
+                                    if pagenow == 1:
+                                        break
+                                    pagenow += 1
+                                continue
+                            else:
+                                log.error(f"# {idx+1}-{pagenow}-{flag+1}: 被认为是机器人")  
+                                continue
+
+                        result_page_contents=self.prase_page_cp_boxes(soup)
+                        self.results[idx]['patent'][pagenow]=result_page_contents
+                        log.info(f' # {idx+1}-{pagenow}-{flag+1}: {company} success\n')
                         
-                    except:
-                        if soup.find("h1", class_="head_title") == None:
-                            log.error(f"# {idx+1}-{pagenow}-{flag+1}: 没有您要查询的结果")
-                            flag+=1
-                            if flag >= self.trytimes:
-                                self.spider_all+=1
-                                log.info(f' # {idx+1}-{pagenow}-{flag}: {company} failed')
-                                idx += 1
-                                flag = 0
-                                t2 = time.time()
-                                log.info(f' # 耗时{round((t2-t1),1)}seconds, 成功爬取了{self.spider_success}/{self.spider_all}家公司\n')
-                                if pagenow == 1:
-                                    break
-                                pagenow += 1
-                            continue
-                        else:
-                            log.error(f"# {idx+1}-{pagenow}-{flag+1}: 被认为是机器人")  
-                            continue
+                        flag = 0
+                        self.spider_success+=1
+                        self.spider_all+=1
 
-                    result_page_contents=self.prase_page_cp_boxes(soup)
-                    self.results[idx]['patent'][pagenow]=result_page_contents
-                    log.info(f' # {idx+1}-{pagenow}-{flag+1}: {company} success\n')
-                    
-                    flag = 0
-                    self.spider_success+=1
-                    self.spider_all+=1
-
-                    with open(self.pklfile, 'wb') as f:
-                        pickle.dump(self.results, f)
-                    log.info(f" # {idx+1}-{pagenow}-{flag+1}: 保存到文件")
-                    t2 = time.time()
-                    log.info(f' # 耗时{t2-t1}seconds, 成功爬取了{self.spider_success}/{self.spider_all}家公司\n')
-                    pagenow += 1
-                    if pagenow == page_size:
-                        idx+=1
-                        break
-                else:
-                    log.info(f' # {idx+1}-{pagenow}-{flag+1}: {company} has successed\n')
-                    flag = 0
-                    pagenow += 1
-                    page_size = self.results[idx]['page_size']
-                    if pagenow == page_size:
-                        idx+=1
-                        break      
+                        with open(self.pklfile, 'wb') as f:
+                            pickle.dump(self.results, f)
+                        log.info(f" # {idx+1}-{pagenow}-{flag+1}: 保存到文件")
+                        t2 = time.time()
+                        log.info(f' # 耗时{t2-t1}seconds, 成功爬取了{self.spider_success}/{self.spider_all}家公司\n')
+                        pagenow += 1
+                        if pagenow == page_size:
+                            idx+=1
+                            break
+                    else:
+                        log.info(f' # {idx+1}-{pagenow}-{flag+1}: {company} has successed\n')
+                        flag = 0
+                        pagenow += 1
+                        page_size = self.results[idx]['page_size']
+                        if pagenow == page_size:
+                            idx+=1
+                            break      
 
 
