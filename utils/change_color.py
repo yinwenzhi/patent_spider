0f384e0b4261c7db1d173a555b45d191b255fa1a
qhykwsw <1425751338@qq.com>
2019-03-19 08:58:28 +0800

    add change color

---
+++ utils/change_color.py
@@ -0,0 +1,8 @@
+def red(string):
+	return "\033[0;31mfailed\033[0m".format(str)
+
+def green(str):
+	return "\033[0;32m{}\033[0m".format(str)
+
+def yellow(str):
+	return "\033[0;33m{}\033[0m".format(str)
